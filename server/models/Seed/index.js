b503d730aa05e06bd79b89dd2bdfbc6ed69ff44b
lewy192 <lewis.james.hill@outlook.com>
2021-09-09 15:14:32 +1000

    add:seed data for gql testing

---
+++ server/models/Seed/index.js
@@ -0,0 +1,163 @@
+const User = require("../User");
+const Message = require("../Message");
+
+const messages = [
+    {
+        targetId: 1,
+        userId: 2,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 1,
+        userId: 2,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 1,
+        userId: 2,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 1,
+        userId: 2,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 1,
+        userId: 2,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 1,
+        userId: 2,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 1,
+        userId: 2,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 1,
+        userId: 2,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 1,
+        userId: 2,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 1,
+        userId: 2,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 1,
+        userId: 2,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 1,
+        userId: 2,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 1,
+        userId: 2,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 1,
+        userId: 2,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 1,
+        userId: 2,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 2,
+        userId: 1,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 2,
+        userId: 1,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 2,
+        userId: 1,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 2,
+        userId: 1,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 2,
+        userId: 1,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 2,
+        userId: 1,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 2,
+        userId: 1,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 2,
+        userId: 1,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 2,
+        userId: 1,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+    {
+        targetId: 2,
+        userId: 1,
+        messageContents:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur doloribus repellendus officiis, nisi, iure enim, labore doloremque vel quo perferendis assumenda velit debitis corporis autem. Explicabo neque saepe similique.",
+    },
+];
+
+const users = [
+    { username: "lewy192", password: "test" },
+    { username: "lewy100", password: "test" },
+];
+
+User.bulkCreate(users);
+Message.bulkCreate(messages);
