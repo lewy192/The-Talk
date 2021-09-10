b8993aef132abc67d754b9e0898b91b814fba775
lewy192 <lewis.james.hill@outlook.com>
2021-09-10 16:21:50 +1000

    add: conditinally render TalkRoom if a recipient user has been selected.

--- client/src/AuthApp.js
+++ client/src/AuthApp.js
@@ -1,26 +1,26 @@
-import React, { lazy, useContext } from "react";
+import React, { lazy, useContext, useState } from "react";
 import { AuthContext } from "./context/authContext";
 import "./AuthApp.scss";
-const ChatHeader = lazy(() => import("./components/ChatHeader/ChatHeader"));
-const ChatBox = lazy(() => import("./components/ChatBox/ChatBox"));
-const ComposeMessage = lazy(() =>
-    import("./components/ComposeMessage/ComposeMessage")
-);
+import TalkRoom from "./containers/TalkRoom/TalkRoom";
+import { ChatContext } from "./context/chatContext";
+
 const Chats = lazy(() => import("./containers/Chats/Chats"));
 const AuthApp = () => {
     const { userState } = useContext(AuthContext);
     const { user } = userState;
-    console.log(user);
+    const { chatState } = useContext(ChatContext);
+    console.log(chatState);
+    console.log("app render");
     return (
         <div className="the-talk-home flex">
             <aside className="chats">
                 <Chats user={user} />
             </aside>
-            {/* <section className="the-talk-room">
-                <ChatHeader />
-                <ChatBox />
-                <ComposeMessage />
-            </section> */}
+            {chatState?.recipientUser ? (
+                <TalkRoom currentUser={user} users={chatState} />
+            ) : (
+                <div className="choose-user">Choose User</div>
+            )}
         </div>
     );
 };
