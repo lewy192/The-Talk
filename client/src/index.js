479c0eb84e6661b1474c86df555bcfec383b4a6f
lewy192 <lewis.james.hill@outlook.com>
2021-09-11 15:59:58 +1000

    add: websocket link to apollo client

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,33 @@ import App from "./App";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { AuthProvider } from "./context/authContext";
 import { ChatProvider } from "./context/chatContext";
+import { split, HttpLink } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { WebSocketLink } from "@apollo/client/link/ws";
+
+const httpLink = new HttpLink({
+    uri: "/graphql",
+});
+
+const wsLink = new WebSocketLink({
+    uri: "/graphql",
+    options: {
+        reconnect: true,
+        reconnectionAttempts: 20,
+    },
+});
+
+const link = split(
+    ({ query }) => {
+        const { kind, opertaion } = getMainDefinition(query);
+        return kind === "OperationDefinition" && opertaion === "subscription";
+    },
+    wsLink,
+    httpLink
+);
 
 const client = new ApolloClient({
-    uri: "/graphql",
+    link,
     cache: new InMemoryCache(),
 });
 
