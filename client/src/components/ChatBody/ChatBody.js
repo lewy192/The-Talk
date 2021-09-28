95b67c6c63c0891b14b67a2c5d06e4826a443986
lewy192 <lewis.james.hill@outlook.com>
2021-09-28 15:24:26 +1000

    add: subscribe to updates on component mount, unsub on unmount

--- client/src/components/ChatBody/ChatBody.js
+++ client/src/components/ChatBody/ChatBody.js
@@ -1,15 +1,64 @@
 import { useQuery } from "@apollo/client";
-import React, { lazy } from "react";
+import React, { lazy, useEffect } from "react";
+import { ALL_MESSAGES } from "../../utils/queries.js";
+import { MESSAGE_CREATED_SUBSCRIPTION } from "../../utils/subscriptions.js";
 
 import "./ChatBody.scss";
 
 const Message = lazy(() => import("./../Message/Message"));
 
 const ChatBody = (props) => {
-    const { recipientUser, currentUser } = props;
-    // const { data: recipientData, error, loading } = useQuery({});
-    // console.log(recipientData);
+    const {
+        recipientUser: { id: targetId },
+        currentUser: { id: userId },
+    } = props;
 
-    return <section className="chat-body">{recipientUser?.username}</section>;
+    const { data, loading, subscribeToMore } = useQuery(ALL_MESSAGES, {
+        variables: { targetId: targetId / 1, userId: userId / 1 },
+    });
+
+    useEffect(() => {
+        return subscribeToMore({
+            document: MESSAGE_CREATED_SUBSCRIPTION,
+            variables: { recipientId: targetId / 1, senderId: userId / 1 },
+            updateQuery: (prev, { subscriptionData }) => {
+                const { newMessage } = subscriptionData.data;
+                if (!subscriptionData) return prev;
+                return {
+                    ...prev,
+                    getTargetMessages: [...prev.getTargetMessages, newMessage],
+                };
+            },
+        });
+    }, [targetId]);
+    const { getTargetMessages: messageArray } = data || {};
+    return (
+        <section className="chat-body">
+            {!loading && messageArray ? (
+                messageArray.map(
+                    (
+                        { messageContents, targetId: messageTargetId, id },
+                        index
+                    ) => {
+                        return messageTargetId === targetId ? (
+                            <Message
+                                messageText={messageContents}
+                                isSent={true}
+                                key={index}
+                            />
+                        ) : (
+                            <Message
+                                messageText={messageContents}
+                                isSent={false}
+                                key={index}
+                            />
+                        );
+                    }
+                )
+            ) : (
+                <p>Start The Chat!</p>
+            )}
+        </section>
+    );
 };
 export default ChatBody;
